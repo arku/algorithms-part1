eb30dac7abcb46c439cd777f8852050089a6c2b7
Arun Kumar <arunkumarm1995@gmail.com>
2016-09-16 23:12:29 +0530

    Implement dequeue, sample and iterator methods

--- Week1/RandomizedQueue/src/RandomizedQueue.java
+++ Week1/RandomizedQueue/src/RandomizedQueue.java
@@ -1,7 +1,13 @@
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
 /**
  * Created by zarak on 2016-09-16.
  */
-public class RandomizedQueue<Item> {
+public class RandomizedQueue<Item> implements Iterable<Item> {
     private int numItems;
     private Item[] array;
 
@@ -17,12 +23,39 @@ public class RandomizedQueue<Item> {
         return numItems;
     }
 
+    public Item getItem(int index) {
+        return array[index];
+    }
+    public int getLength() {
+        return array.length;
+    }
+
     public void enqueue(Item item) {
         if (item == null) throw new NullPointerException();
         if (numItems == array.length) {
             resize(2 * numItems);
         }
         array[numItems++] = item;
+    }
+
+    public Item dequeue() {
+        if (isEmpty()) throw new NoSuchElementException();
+        if (size() == array.length / 4) resize(array.length / 2);
+        int randIndex = StdRandom.uniform(numItems);
+
+        Item item = array[randIndex];
+        array[randIndex] = array[--numItems];
+        array[numItems] = null;
+
+        return item;
+
+    }
+
+    public Item sample() {
+        if(isEmpty()) throw new NoSuchElementException();
+
+        int randIndex = StdRandom.uniform(numItems);
+        return array[randIndex];
     }
 
     private void resize(int size) {
@@ -32,5 +65,51 @@ public class RandomizedQueue<Item> {
         array = copy;
     }
 
+    public Iterator<Item> iterator() {
+        return new QueueIterator();
+    }
+    
+    class QueueIterator implements Iterator {
+        private int current;
+        private Item[] shuffled = (Item[]) new Object[numItems];
 
+        public QueueIterator() {
+            for(int i = 0; i < numItems; i++) {
+                shuffled[i] = array[i];
+            }
+            StdRandom.shuffle(shuffled);
+        }
+
+        public boolean hasNext() {
+            return current < numItems;
+        }
+
+        public Item next() {
+            return shuffled[current++];
+        }
+
+    }
+
+    public static void main(String[] args) {
+        RandomizedQueue<String> queue = new RandomizedQueue<>();
+        queue.enqueue("a");
+        queue.enqueue("b");
+        queue.enqueue("c");
+        queue.enqueue("d");
+        queue.enqueue("e");
+
+        for(int i = 0; i < 4; i++) {
+            for (String s : queue)
+                StdOut.print(s + " ");
+            StdOut.println();
+        }
+
+        queue.dequeue();
+        queue.dequeue();
+        queue.dequeue();
+        queue.dequeue();
+        queue.dequeue();
+        for (String s : queue)
+            StdOut.print(s + " ");
+    }
 }
--- Week1/RandomizedQueue/tests/RandomizedQueueTest.java
+++ Week1/RandomizedQueue/tests/RandomizedQueueTest.java
@@ -1,6 +1,8 @@
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.NoSuchElementException;
+
 import static org.junit.Assert.*;
 
 /**
@@ -17,7 +19,52 @@ public class RandomizedQueueTest {
     @Test
     public void enqueueTest(){
         queue.enqueue("a");
+
+        assertEquals(1, queue.getLength());
         assertEquals(1, queue.size());
-        //assertEquals("a",);
+        assertEquals("a", queue.getItem(0));
+
+        queue.enqueue("b");
+        assertEquals(2, queue.getLength());
+
+        queue.enqueue("c");
+        assertEquals(4, queue.getLength());
     }
-}
+
+    @Test(expected = NoSuchElementException.class)
+    public void dequeueExceptionTest() {
+        queue.dequeue();
+    }
+
+    @Test
+    public void dequeueTest() {
+        queue.enqueue("a");
+        queue.enqueue("b");
+        queue.enqueue("c");
+        queue.enqueue("d");
+        queue.enqueue("e");
+
+        assertEquals(5, queue.size());
+        String removed = queue.dequeue();
+        assertNotNull(removed);
+        assertEquals(4, queue.size());
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void sampleExceptionTest() {
+        queue.sample();
+    }
+
+    @Test
+    public void sampleTest() {
+        queue.enqueue("a");
+        queue.enqueue("b");
+        queue.enqueue("c");
+
+        assertEquals(3, queue.size());
+        String sample = queue.sample();
+        assertNotNull(sample);
+        assertEquals("Size shouldn't change", 3, queue.size());
+
+    }
+ }
