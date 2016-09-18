7b3a86c585dfece2ffb9abde084851d5babb5617
Arun Kumar <arunkumarm1995@gmail.com>
2016-09-18 23:17:17 +0530

    Implement subset client

---
+++ Week1/RandomizedQueue/src/Subset.java
@@ -0,0 +1,24 @@
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
+/**
+ * Created by zarak on 2016-09-18.
+ */
+public class Subset {
+    public static void main(String[] args) {
+        int k = Integer.parseInt(args[0]);
+        int counter  = 0;
+
+        RandomizedQueue<String> rq = new RandomizedQueue<>();
+        while (!StdIn.isEmpty()) {
+            rq.enqueue(StdIn.readString());
+        }
+        
+        for (String s: rq) {
+            if (counter++ < k)
+                StdOut.print(s + " ");
+            else
+                break;
+        }
+    }
+}
